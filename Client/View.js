2979a2513ba3141af16360856fc212669c1da561
Ao Zeng <13871299198@msn.cn>
2017-04-10 02:16:39 -0400

    as it seems

--- Client/View.js
+++ Client/View.js
@@ -1,12 +1,27 @@
 var url = "";
 
 function isBook(current){
+    // take the current path of the content inside iframe;
+    // if current path is a book, m will be a list of string,
+    // where m[1] = the title of the book, m[2] = the current page num
     var re=/^\/\d+\/\d+\/\d+\/([^\/]+)\/(?:(\d+)\/)?(?:\?.*)?$/,
 	m=current.match(re);
-    alert(m);
+    addGrid(m);
+}
+
+function addGrid(bookInfo){
+    $('#left_grid').append(
+        '<tr><th><img src="/CHAIR/PCS/00105all.png" alt="test image" style="width:304px;height:228px;"></th>'
+			+ '<th><img src="/CHAIR/PCS/00105all.png" alt="test image" style="width:304px;height:228px;"></th>'
+			+ '<th><img src="/CHAIR/PCS/00105all.png" alt="test image" style="width:304px;height:228px;"></th>'
+			+ '<th><img src="/CHAIR/PCS/00105all.png" alt="test image" style="width:304px;height:228px;"></th>'
+			+ '</tr>'
+    );
 }
 
 $(document).ready(
+    // The following function constantly check the current path
+    // If path is changed, call function isBook
     setInterval(function(){
 		var current=$('#my_iframe').get(0).contentWindow.location.pathname;
 		if(current != url){
