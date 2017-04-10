7fcdb10641a7a8080ffa37cf82a2c7b3fe3c8985
Ao Zeng <13871299198@msn.cn>
2017-04-10 02:10:04 -0400

    as it seems

--- Client/View.js
+++ Client/View.js
@@ -1,12 +1,18 @@
 var url = "";
+
+function isBook(current){
+    var re=/^\/\d+\/\d+\/\d+\/([^\/]+)\/(?:(\d+)\/)?(?:\?.*)?$/,
+	m=current.match(re);
+    alert(m);
+}
+
 $(document).ready(
     setInterval(function(){
 		var current=$('#my_iframe').get(0).contentWindow.location.pathname;
 		if(current != url){
 			console.log('change', current);
-            alert("changed");
+            isBook(current);
 			url=current;
 		}
 	}, 200)
-
 );
