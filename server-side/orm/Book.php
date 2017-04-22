c1167a9d43aa017e062de3b9218f0a5c98f0ffbf
Ao Zeng <13871299198@msn.cn>
2017-04-22 13:49:41 -0400

    debuggin

--- server-side/orm/Book.php
+++ server-side/orm/Book.php
@@ -60,9 +60,7 @@ class Book{
     }
 
     public function get_json(){
-        echo var_dump("name: ".$this->bookName." ,pages: ".$this->pages."<br>");
-        $restul_array = array(
-            'name' => $this->bookName,
+        $result_array = array(
             'pages' => $this->pages
         );
         return json_encode($result_array);
