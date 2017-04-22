31ca60afd0334b9a25844377986ab12dec562600
Ao Zeng <13871299198@msn.cn>
2017-04-22 13:51:01 -0400

    debuggin

--- server-side/orm/Book.php
+++ server-side/orm/Book.php
@@ -43,7 +43,7 @@ class Book{
             $annoId = $result_row['annoId'];
             //find the range of pages
             $result = $mysqli->query(
-                "select max(Annotatin.pageNum) from Annotation where Annotation.bookId =".intval($id)
+                "select max(Annotation.pageNum) from Annotation where Annotation.bookId =".intval($id)
             );
             $result_row = $result->fetch_array();
             $max_page_num = intval($result_row['pagenum']);
