fb8b09c6428937fc60241e9c136b57fb41b2a5c4
Ao Zeng <13871299198@msn.cn>
2017-04-22 13:34:08 -0400

    trivial change

--- server-side/hub.php
+++ server-side/hub.php
@@ -19,7 +19,7 @@ ini_set('display_errors', 1);
         echo var_dump($_GET["bookId"])."<br>";
         $bookId = intval($_GET["bookId"]);
         echo var_dump($bookId)."<br>";
-        echo var_dump($bookId == null)."<br>";
+        echo var_dump(is_null($bookId))."<br>";
         if($bookId == null){
             header("HTTP/1.0 404 NOT FOUND");
             print("Id: ".$bookId." Not Found");
