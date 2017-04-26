a8441ff32f8794a8bda9b03a8522e5b3d153d2f2
Ao Zeng <13871299198@msn.cn>
2017-04-26 10:14:18 -0400

    selector fixed

--- Client/BookList.js
+++ Client/BookList.js
@@ -9,14 +9,16 @@ var BookList = function(bookList_json){
 BookList.prototype.fill_notation = function(){
     // fill the Notation dropdown on the navbar
     // with all the notation it gets
+    var id_array = this.id_array;
+    var name_array = this.name_array;
     var dropDown = $("#NotationDropDown ul");
     // clear the previous elements in the nav
     dropDown.html('');
     for(var i=0; i<this.len; i++){
         var tmp_li = $('<li><a></a></li>');
         // bind the book id with the li
-        tmp_li.data("id", this.id_array[i]);
-        tmp_li.html(this.name_array[i]);
+        tmp_li.data("id",id_array[i]);
+        tmp_li.html(name_array[i]);
         dropDown.append(tmp_li);
     }
 }
