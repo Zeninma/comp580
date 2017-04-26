d0caf2248e54f0ddd3af6bb0dcfb979a75317463
Ao Zeng <13871299198@msn.cn>
2017-04-26 13:29:28 -0400

    debugging

--- Client/View.js
+++ Client/View.js
@@ -66,7 +66,8 @@ function loadNotation(page_num){
         var text = tmp_symbol.words;
         var curr_td = $('#grid'+i);
         curr_td.html('<img src = "'+pic+'" alt = "pics" style = "width: 100%">');
-        $('#grid'+i+' img').on('click',
+        var curr_img = $('#grid'+i+' img');
+        curr_img.on('click',
         responsiveVoice.speak(text));
     }
 }
