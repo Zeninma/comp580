db249bec5b70c0f119bb46ab78285716a406606e
Ao Zeng <13871299198@msn.cn>
2017-04-26 00:08:07 -0400

    fixed the iteration for arrays

--- Client/Book.js
+++ Client/Book.js
@@ -5,7 +5,7 @@ var Book = function(book_json){
     //field pages is an array that contains Page object.
     this.pages = new Array();
     
-    for (var i = 0; i < book.pages.length; i++){
+    for (var i = 0; i < book_json.pages.length; i++){
         // initialization of field pages
         tmp = new Page(book_json.pages[i]);
         this.pages.push(tmp);
