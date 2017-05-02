9e43879f22592daf7263673e051d273930379e9a
Ao Zeng <13871299198@msn.cn>
2017-05-01 23:51:45 -0400

    trivial

--- Client/View.js
+++ Client/View.js
@@ -31,12 +31,15 @@ function start(){
     setInterval(function(){
         // set the interval to check the current path
 		var current=$('#my_iframe').get(0).contentWindow.location.pathname;
-        if(current_name!=''){
+        // dynamically change the layout
+        var re=/^\/\d+\/\d+\/\d+\/([^\/]+)\/(?:(\d+)\/)?(?:\?.*)?$/,
+        m=current.match(re);
+        if(!m){
+            noBookLayout();
+        }else{
             bookLayout();
         }
-        else{
-            noBookLayout();
-        }
+
 		if(current != url){
 			console.log('change', current);
             isBook(current);
