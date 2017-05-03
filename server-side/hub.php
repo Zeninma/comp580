663b9a4e854e738910b3ae78efe869334fec55ab
Ao Zeng <13871299198@msn.cn>
2017-05-03 14:04:50 -0400

    debug

--- server-side/hub.php
+++ server-side/hub.php
@@ -57,8 +57,8 @@ ini_set('display_errors', 1);
                 $annotations = json_decode($_GET['annotations']);
                 $book_name = $_GET['name'];
                 $anno_name = $_GET['annoName'];
-                echo var_dump($annotations[0])+'<br>';
-                Book::addNewBook($book_name, $anno_name,$annotations[0]);
+                echo var_dump($annotations->annotations)+'<br>';
+                Book::addNewBook($book_name, $anno_name,$annotations->annotations);
             }
             else{
                 header("HTTP/1.0 404 NOT FOUND");
